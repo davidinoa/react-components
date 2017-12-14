eb4f3f0baea60b7c5050c9effa453ec3713f130c
David Inoa <davidinoa@gmail.com>
2017-12-14 10:16:06 -0500

    Create component for each GroceryList item

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+var Fruits = () => (
+  <li>fruits</li>
+);
+
+var Vegetables = () => (
+  <li>vegetables</li>
+);
+
 var GroceryList = () => (
   <ul>
-    <li>'fruits'</li>
-    <li>'vegetables'</li>
+    <Fruits />
+    <Vegetables />
   </ul>
 );
 
